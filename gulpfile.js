8151a014cc0536102a1c88407921f7e9e21abb7c
Mike Pham <mpham@nativecode.com>
2016-06-05 08:50:48 -0400

    packaging

--- gulpfile.js
+++ gulpfile.js
@@ -43,7 +43,7 @@ gulp.bt.reload('build').when({
 
 gulp.task('default', ['build'])
 
-gulp.task('package', ['build'], plugin.shell.task(['su --session-command="python tools/apkg-tools.py create dist" dynamic']))
+gulp.task('package', ['build'], plugin.shell.task(['echo -e "\n"|sudo -S python tools/apkg-tools.py create dist']))
 
 gulp.task('download', ['plexpy'])
 gulp.task('plexpy', done => {
