a4c4f5be64e7216e1fdb94af3ad957f68e629683
Mike Pham <mpham@nativecode.com>
2016-06-11 19:21:45 -0400

    removing tee

--- src/CONTROL/post-install.sh
+++ src/CONTROL/post-install.sh
@@ -3,7 +3,7 @@
 DATADIR=/share/{{name}}
 PACKAGE=/usr/local/AppCentral/{{name}}
 
-git clone {{{plexpy.url}}} $PACKAGE/lib | tee $PACKAGE/git.log
+git clone {{{plexpy.url}}} $PACKAGE/lib
 cd $PACKAGE/lib
 git checkout v{{version}}
 git branch -D master
