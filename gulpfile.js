ee768a53d89c3593b6f9a8d8d48a73ec2227fb77
Mike Pham <mpham@nativecode.com>
2016-06-11 18:06:52 -0400

    fixing clean script

--- gulpfile.js
+++ gulpfile.js
@@ -69,4 +69,5 @@ gulp.task('clean', () => {
 })
 
 gulp.task('default', ['build'])
-gulp.task('package', ['build'], plugin.shell.task(['echo -e "\n" | sudo -S python tools/apkg-tools.py create dist']))
+gulp.task('package', ['package:build'], plugin.shell.task(['echo -e "\n" | sudo -S python tools/apkg-tools.py create dist']))
+gulp.task('package:build', ['clean'], () => gulp.start('build'))
