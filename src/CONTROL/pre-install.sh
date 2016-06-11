69b97d443e0f89d95e9db2e4ebcbf72a0a80ea02
Mike Pham <mpham@nativecode.com>
2016-06-11 18:03:43 -0400

    changing script to pre-install

--- src/CONTROL/pre-install.sh
+++ src/CONTROL/pre-install.sh
@@ -3,11 +3,11 @@
 DATADIR=/share/{{name}}
 PACKAGE=/usr/local/AppCentral/{{name}}
 
-if [ ! -d "$DATADIR" ]; then
+if [ ! -d $DATADIR ]; then
     mkdir -p $DATADIR
 fi
 
-if [ ! -d "$PACKAGE/lib"]; then
+if [ ! -d $PACKAGE/lib]; then
     git clone {{{plexpy.url}}} $PACKAGE/lib
 fi
 
