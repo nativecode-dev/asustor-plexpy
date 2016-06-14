feeae5ad549453fe0ab79598adf4c11b8163e6ff
Mike Pham <mpham@nativecode.com>
2016-06-13 23:45:37 -0400

    improvements to post-install.sh

--- src/CONTROL/post-install.sh
+++ src/CONTROL/post-install.sh
@@ -3,8 +3,33 @@
 DATADIR=/share/{{name}}
 PACKAGE=/usr/local/AppCentral/{{name}}
 
-git clone {{{plexpy.url}}} $PACKAGE/lib
-cd $PACKAGE/lib
-git checkout v{{version}}
-git branch -D master
-git checkout -b master
+GROUP=administrators
+USER=admin
+
+case $APKG_PKG_STATUS in
+    "install"|"upgrade")
+        if [ ! -d "$PACKAGE/lib"]; then
+            git clone {{{plexpy.url}}} $PACKAGE/lib
+        fi
+
+        cd $PACKAGE/lib
+        git checkout v{{version}}
+        git branch -D master
+        git checkout -b master
+
+        # Set ownership to admin/administrators, per the docs.
+        # http://developer.asustor.com
+        chown $USER:$GROUP $PACKAGE/lib -R
+
+        exit $?
+    ;;
+
+    "uninstall")
+        rm -rf $PACKAGE/lib
+        exit $?
+    ;;
+
+    *)
+        exit 1
+    ;;
+esac
