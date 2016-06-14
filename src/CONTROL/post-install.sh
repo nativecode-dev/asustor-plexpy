1e9b3c98313e1fee03d541dcf79db6ef639f20bd
Mike Pham <mpham@nativecode.com>
2016-06-14 00:00:37 -0400

    fixing post-install to not use the upgrade flag

--- src/CONTROL/post-install.sh
+++ src/CONTROL/post-install.sh
@@ -6,25 +6,15 @@ PACKAGE=/usr/local/AppCentral/{{name}}
 GROUP=administrators
 USER=admin
 
-case $APKG_PKG_STATUS in
-    "install"|"upgrade")
-        if [ ! -d "$PACKAGE/lib"]; then
-            git clone {{{plexpy.url}}} $PACKAGE/lib
-        fi
+if [ ! -d "$PACKAGE/lib"]; then
+    git clone {{{plexpy.url}}} $PACKAGE/lib
+fi
 
-        cd $PACKAGE/lib
-        git checkout v{{version}}
-        git branch -D master
-        git checkout -b master
+cd $PACKAGE/lib
+git checkout v{{version}}
+git branch -D master
+git checkout -b master
 
-        # Set ownership to admin/administrators, per the docs.
-        # http://developer.asustor.com
-        chown $USER:$GROUP $PACKAGE/lib -R
-
-        exit $?
-    ;;
-
-    *)
-        exit 1
-    ;;
-esac
+# Set ownership to admin/administrators, per the docs.
+# http://developer.asustor.com
+chown $USER:$GROUP $PACKAGE/lib -R
